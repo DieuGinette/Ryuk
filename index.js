6955a96f4b671d203951a29861300299117adc38
Nathan BLANCHET <nathanblanchet@ordinatdenathan.home>
2023-11-15 18:35:38 +0100

    Création de l'handler events + ajout des events MessageCreate et ready.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const { Client, IntentsBitField, Collection, messageLink } = require("discord.js");
 const client = new Client({ intents: new IntentsBitField(3276799) });
 const loadCommands = require("./loaders/loadCommands");
+const loadEvents = require("./loaders/loadEvents");
 const dotenv = require("dotenv");
 dotenv.config();
 
 client.commands = new Collection();
 
 loadCommands(client);
-
-client.on("messageCreate", (message) => {
-    const prefix = "!";
-    if (!message.content.startsWith(prefix)) return;
-
-    const arrayMessage = message.content.split(" ");
-    const name = arrayMessage[0].slice(prefix.length, arrayMessage[0].length);
-    const command = client.commands.get(name);
-
-    command.run(client, message)
-});
+loadEvents(client);
 
 client.login(process.env.TOKEN);
---
+++ loaders/loadEvents.js
@@ -0,0 +1,16 @@
+const { readdirSync } = require("fs");
+
+module.exports = client => {
+    let count = 0;
+    const dirsEvent = readdirSync("./events/");
+    for (const dirs of dirsEvent) {
+        const fileDirs = readdirSync(`./events/${dirs}/`).filter(f => f.endsWith(".js"));
+        for (const file of fileDirs) {
+            const event = require(`../events/${dirs}/${file}`);
+            client.on(event.name, (...args) => event.run(client, ...args));
+            count++;
+        };
+    };
+
+    console.log(`[${count} events]`);
+};
