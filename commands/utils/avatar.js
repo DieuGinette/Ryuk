05d321b551151e4998d3a50c5219bc4e6f0a70ed
Nathan BLANCHET <nathanblanchet@MBP-de-Nathan.lan>
2023-11-17 22:23:52 +0100

    Ajout de la commande avatar.js

---
+++ commands/utils/avatar.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("avatar")
+        .setDescription("Renvoie l'avatar d'un utilisateur")
+        .setDMPermission(true)
+        .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
+        .addUserOption(option =>
+            option.setName('utilisateur')
+                .setDescription("L'utilisateur dont vous voulez obtenir l'avatar")
+                .setRequired(true)),
+
+    async run(interaction) {
+        const user = interaction.options.getUser('utilisateur') || interaction.user;
+
+        const avatarUrl = user.displayAvatarURL({ dynamic: true, size: 512 });
+
+        const embed = new EmbedBuilder()
+            .setTitle(`Avatar de ${user.tag}`)
+            .setImage(avatarUrl)
+            .setColor("#7289DA")
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed] });
+    }
+};
